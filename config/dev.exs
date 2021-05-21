12d38fc22d47d6919bbbb644e36e09af9ce58e85
Rebecca Le <traybaby@gmail.com>
2021-05-21 16:34:09 +0800

    Set up JIT compilation in Tailwind CSS

--- config/dev.exs
+++ config/dev.exs
@@ -21,11 +21,9 @@ config :tailwind_jit, TailwindJitWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
+    npm: [
+      "run",
+      "watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
